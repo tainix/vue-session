69ef6d06838237b5e9d2c8c0f4a5ea89be44d228
Tainix <9879474+tainix@users.noreply.github.com>
2018-05-29 13:04:19 +0800

    Update index.js
    remove installed flag, because Vue has do it.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import Session from './Session'
 import SessionInput from './session-input.vue'
 
-let installed
-
 function install(Vue, options) {
-	if(installed) {
-		return
-	}
-
-	installed = true
 	const session = new Session(options || {})
 
 	Vue.$session = session
