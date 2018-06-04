218f6ce99369d2850765c77361ea6f4fbd01ffdf
tainix.li <tainix.li@shareworks.cn>
2018-06-04 16:02:41 +0800

    webpack read package.json's name

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const fs = require('fs');
+const libName = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'))).name
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
-		library: 'sd-vue-session',
+		library: libName,
 		libraryTarget: 'umd'
 	},
 	module: {
